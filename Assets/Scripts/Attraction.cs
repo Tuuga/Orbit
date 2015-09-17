fa1a6a3a9b5224091eb12e213bcc04ed1a8c22ce
Tuuga <tuukka.o.muukkonen@gmail.com>
2015-09-17 15:09:46 +0300

    Fabric and physics

---
+++ Assets/Scripts/Attraction.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attraction : MonoBehaviour {
+
+	GameObject player;
+	GameObject[] planets;
+
+	public float mass = 5.0f;
+	float strength;
+
+	void Start () {
+
+		player = GameObject.Find ("Player");
+		planets = GameObject.FindGameObjectsWithTag("Planet");
+		strength = mass;
+	}
+
+	void Gravity(GameObject g) {
+
+		mass = strength / Vector2.Distance (g.transform.position, transform.position);
+
+		Vector3 direction = transform.position - g.transform.position;
+		g.GetComponent<Rigidbody>().AddForce(mass * direction);
+	}
+	
+	void FixedUpdate () {
+
+		Gravity (player);
+
+		for (int i = 0; i < planets.Length; i++) {
+			Gravity (planets[i]);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float pushStr;
+	Rigidbody rb;
+	
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+	
+		//WASD Controls
+		if (Input.GetKey (KeyCode.W)) {
+			rb.AddForce (pushStr * Vector2.up);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			rb.AddForce (pushStr * Vector2.left);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			rb.AddForce (pushStr * Vector2.down);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			rb.AddForce (pushStr * Vector2.right);
+		}
+	}
+}
