11b0e2bbcdfdd669d0c7cf646e0786513ffff0c4
Tuukka Muukkonen <tuukka.o.muukkonen@gmail.com>
2015-09-18 17:18:37 +0300

    Physics testing

--- Assets/Scripts/Attraction.cs
+++ Assets/Scripts/Attraction.cs
@@ -6,6 +6,8 @@ public class Attraction : MonoBehaviour {
 	GameObject player;
 	GameObject[] planets;
 
+	bool canAttract;
+
 	public float mass = 5.0f;
 	float strength;
 
@@ -13,23 +15,32 @@ public class Attraction : MonoBehaviour {
 
 		player = GameObject.Find ("Player");
 		planets = GameObject.FindGameObjectsWithTag("Planet");
-		strength = mass;
 	}
 
 	void Gravity(GameObject g) {
 
-		mass = strength / Vector2.Distance (g.transform.position, transform.position);
+		strength = mass / Vector2.Distance (g.transform.position, transform.position);
 
 		Vector3 direction = transform.position - g.transform.position;
-		g.GetComponent<Rigidbody>().AddForce(mass * direction);
+		g.GetComponent<Rigidbody>().AddForce(strength * direction);
 	}
 	
 	void FixedUpdate () {
 
-		Gravity (player);
+		if (canAttract) {
+			Gravity (player);
+		}
 
 		for (int i = 0; i < planets.Length; i++) {
 			Gravity (planets[i]);
 		}
+	}
+
+	void OnTriggerEnter (Collider c) {
+		canAttract = true;
+	}
+
+	void OnTriggerExit (Collider c) {
+		canAttract = false;
 	}
 }
