f2d9013a7370d660682b9eb85ee6dfdb6d1f87db
Tuuga <tuukka.o.muukkonen@gmail.com>
2015-09-21 15:26:41 +0300

    More showing data in PlanetProperties, Attraction (strenght = mass squared / distance), Orbit notes.txt in assets folder

--- Assets/Scripts/Attraction.cs
+++ Assets/Scripts/Attraction.cs
@@ -8,11 +8,13 @@ public class Attraction : MonoBehaviour {
 	
 	void Gravity(GameObject g) {
 
-		strength = mass / Vector2.Distance (g.transform.position, transform.position);
+		strength = Mathf.Sqrt (mass) / Vector2.Distance (g.transform.position, transform.position);
 
 		Vector3 direction = transform.position - g.transform.position;
 		g.GetComponent<Rigidbody>().AddForce(strength * direction);
-		Debug.DrawLine (transform.position, g.transform.position);
+
+//		Debug.DrawLine (transform.position, g.transform.position, Color.red);
+//		Debug.DrawLine (transform.position, direction * strength, Color );
 	}
 
 	void OnTriggerStay (Collider c) {
--- Assets/Scripts/PlanetProperties.cs
+++ Assets/Scripts/PlanetProperties.cs
@@ -6,10 +6,15 @@ public class PlanetProperties : MonoBehaviour {
 	public bool orbiting;
 	public GameObject orbitingAround;
 	public float startForce;
+	public float divider;
 
 	//public for debug
 	public float startDist;
 	public float currentDist;
+	public float maxDist;
+	public float minDist;
+	public float avarageDist;
+
 	Rigidbody rb;
 
 
@@ -19,11 +24,10 @@ public class PlanetProperties : MonoBehaviour {
 
 		if (orbiting) {
 
-
-
 			startDist = Vector2.Distance(transform.position, orbitingAround.transform.position);
+			minDist = startDist;
 			transform.LookAt (orbitingAround.transform, Vector3.back);
-
+			startForce = startDist / divider;
 			rb.AddForce (transform.right * startForce, ForceMode.Impulse);
 
 		}
@@ -32,6 +36,13 @@ public class PlanetProperties : MonoBehaviour {
 	void Update () {
 		if (orbiting) {
 			currentDist = Vector2.Distance(transform.position, orbitingAround.transform.position);
+			if (maxDist < currentDist) {
+				maxDist = currentDist;
+			}
+			if (currentDist < minDist) {
+				minDist = currentDist;
+			}
+			avarageDist = (minDist + maxDist) / 2;
 		}
 	}
 }
