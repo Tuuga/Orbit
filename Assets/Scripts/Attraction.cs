676dceacb869de19859fc06866a1496227eba73b
Tuuga <tuukka.o.muukkonen@gmail.com>
2015-09-24 15:05:43 +0300

    Orbit working

--- Assets/Scripts/Attraction.cs
+++ Assets/Scripts/Attraction.cs
@@ -9,7 +9,6 @@ public class Attraction : MonoBehaviour {
 	public float angle;
 	public Vector3 direction;
 
-	Vector3 velocity;
 	Rigidbody rb;
 	GameObject gs;
 	public bool drawDebugLines;
@@ -18,41 +17,36 @@ public class Attraction : MonoBehaviour {
 	
 	}
 
-	void Gravity(GameObject g) {
+    void Gravity(GameObject g) {
 
-		rb = g.GetComponent<Rigidbody> ();
-		
-		if (g.transform.FindChild ("GravitySource") != null) {
-			gs = g.transform.FindChild("GravitySource").gameObject;
-			mass2 = gs.GetComponent<Attraction>().mass1;
-		}
-        
-		if (gs != null) {
-			force = (mass1 * mass2) / Mathf.Sqrt (Vector2.Distance (g.transform.position, transform.position));
-		} else {
-			//force = mass1 / Mathf.Sqrt (Vector2.Distance (g.transform.position, transform.position));
-		}
+        rb = g.GetComponent<Rigidbody>();
 
-		direction = transform.position - g.transform.position;
-		rb.AddForce(force * direction.normalized);
+        if (g.transform.FindChild("GravitySource") != null) {
+            gs = g.transform.FindChild("GravitySource").gameObject;
+            mass2 = gs.GetComponent<Attraction>().mass1;
+        }
+
+        if (gs != null) {
+            float dist = Vector2.Distance(g.transform.position, transform.position);
+            force = (mass1 * mass2) /  (dist * dist);
+        } else {
+            //force = mass1 / Mathf.Sqrt (Vector2.Distance (g.transform.position, transform.position));
+        }
+
+        direction = (transform.position - g.transform.position).normalized * force;
+
+        rb.AddForce(direction);
 
 		//Debug lines and Angle
 		if (drawDebugLines) {
 			Debug.DrawLine (g.transform.position, rb.velocity, Color.red);
-			Debug.DrawLine (g.transform.position, direction, Color.black);
+			Debug.DrawLine (g.transform.position, force * direction.normalized, Color.black);
 		}
-//		Debug.DrawLine (g.transform.position, rb.velocity + direction * force);
 
-		velocity = rb.velocity;
-
-//		Debug.DrawRay (g.transform.position, direction, Color.yellow);
-//		Debug.DrawRay (g.transform.position, velocity, Color.blue);
-		angle = Vector3.Angle (velocity, direction);
-
+		angle = Vector3.Angle (rb.velocity, direction);
 	}
 
 	void OnTriggerStay (Collider c) {
-        Debug.Log(c.name + ": trigger");
 		if (c.GetComponent<Rigidbody>() != null) {
 			Gravity (c.gameObject);
 		}
--- Assets/Scripts/PlanetProperties.cs
+++ Assets/Scripts/PlanetProperties.cs
@@ -24,17 +24,33 @@ public class PlanetProperties : MonoBehaviour {
 
 		if (orbiting) {
 
-			startDist = Vector2.Distance(transform.position, orbitingAround.transform.position);
-			minDist = startDist;
-			transform.LookAt (orbitingAround.transform, Vector3.back);
-			startForce = startDist / divider;
-			rb.AddForce (transform.right * startForce, ForceMode.Impulse);
+            float mass = orbitingAround.GetComponent<Attraction>().mass1;
+            Vector3 dir = (orbitingAround.transform.position - transform.position).normalized;
 
-		}
+            startDist = Vector2.Distance(transform.position, orbitingAround.transform.position);
+			minDist = startDist;
+
+            Vector3 newDir = Quaternion.AngleAxis (90, Vector3.forward) * dir * Mathf.Sqrt(mass / startDist);
+            Debug.Log(newDir);
+            rb.velocity = newDir;
+
+        }
 	}
 
 	void FixedUpdate () {
+
 		if (orbiting) {
+
+            Vector3 dir = orbitingAround.GetComponent<Attraction>().direction;
+            float force = orbitingAround.GetComponent<Attraction>().force;
+
+            Vector3 newDir = Quaternion.AngleAxis(90, Vector3.forward) * dir;
+            newDir = newDir.normalized * dir.magnitude;
+
+            //rb.velocity = newDir;
+
+            Debug.DrawRay(transform.position, newDir, Color.blue);
+            
 
 			currentDist = Vector2.Distance(transform.position, orbitingAround.transform.position);
 			if (maxDist < currentDist) {
