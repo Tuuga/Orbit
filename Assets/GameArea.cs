3fe5ca0a9fd984a8bb15c43fbb4934aee326947d
Tuuga <tuukka.o.muukkonen@gmail.com>
2015-10-07 14:17:43 +0300

    Game Area trigger collider (debug.log)

---
+++ Assets/GameArea.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameArea : MonoBehaviour {
+
+	void OnTriggerStay (Collider c) {
+
+	}
+
+	void OnTriggerExit (Collider c) {
+		if (c.name == "Player") {
+			Debug.Log("Out of Borders!");
+		}
+	}
+}
