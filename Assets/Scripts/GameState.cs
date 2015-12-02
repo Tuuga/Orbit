cdc93913cb275205501c216fed808774933cae4f
Tuukka Muukkonen <tuukka.o.muukkonen@gmail.com>
2015-12-03 00:09:24 +0200

    Revert "rayplane inavctive on pause and death menu + bigger pause button with separate image"
    This reverts commit 5ea74c7930918fa9bb5fe001bedfe1db4baabb44.

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -6,7 +6,6 @@ public class GameState : MonoBehaviour {
 	public GameObject playingCanvas;
 	public GameObject pauseCanvas;
 	public GameObject deathCanvas;
-	public GameObject rayPlane;
 
 	void Start () {
 		GameObject.Find("ScoreManager").GetComponent<ScoreScript>().Restart();
@@ -20,14 +19,12 @@ public class GameState : MonoBehaviour {
 		playingCanvas.SetActive(false);
 		pauseCanvas.SetActive(true);
 		deathCanvas.SetActive(false);
-		rayPlane.SetActive(false);
 		Time.timeScale = 0;
 	}
 	public void Resume () {
 		playingCanvas.SetActive(true);
 		pauseCanvas.SetActive(false);
 		deathCanvas.SetActive(false);
-		rayPlane.SetActive(true);
 		Time.timeScale = 1;
 	}
 	public void Quit () {
@@ -37,7 +34,6 @@ public class GameState : MonoBehaviour {
 		playingCanvas.SetActive(false);
 		pauseCanvas.SetActive(false);
 		deathCanvas.SetActive(true);
-		rayPlane.SetActive(false);
 		Time.timeScale = 0;
 	}
 }
