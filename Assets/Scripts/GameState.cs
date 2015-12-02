5ea74c7930918fa9bb5fe001bedfe1db4baabb44
Tuukka Muukkonen <tuukka.o.muukkonen@gmail.com>
2015-12-03 00:08:49 +0200

    rayplane inavctive on pause and death menu + bigger pause button with separate image

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -6,6 +6,7 @@ public class GameState : MonoBehaviour {
 	public GameObject playingCanvas;
 	public GameObject pauseCanvas;
 	public GameObject deathCanvas;
+	public GameObject rayPlane;
 
 	void Start () {
 		GameObject.Find("ScoreManager").GetComponent<ScoreScript>().Restart();
@@ -19,12 +20,14 @@ public class GameState : MonoBehaviour {
 		playingCanvas.SetActive(false);
 		pauseCanvas.SetActive(true);
 		deathCanvas.SetActive(false);
+		rayPlane.SetActive(false);
 		Time.timeScale = 0;
 	}
 	public void Resume () {
 		playingCanvas.SetActive(true);
 		pauseCanvas.SetActive(false);
 		deathCanvas.SetActive(false);
+		rayPlane.SetActive(true);
 		Time.timeScale = 1;
 	}
 	public void Quit () {
@@ -34,6 +37,7 @@ public class GameState : MonoBehaviour {
 		playingCanvas.SetActive(false);
 		pauseCanvas.SetActive(false);
 		deathCanvas.SetActive(true);
+		rayPlane.SetActive(false);
 		Time.timeScale = 0;
 	}
 }
